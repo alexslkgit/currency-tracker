8076417929302aeffdce0197de6abaf86f1afa31
alexslk <alexdneprin@icloud.com>
2025-04-21 08:48:46 +0200

    [CT] Project initialized.

---
+++ Currency tracker/Currency tracker/Currency_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_trackerApp.swift
+//  Currency tracker
+//
+//  Created by Slobodianiuk Oleksandr on 21.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Currency_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
